c596ecb436e525080649356784c4533444cfc5c2
Farookh-Mansuri <farookh@coforge.com>
2022-09-20 11:27:17 +0530

    assignment4 update

--- GoTrainingProgram/Assignments/Assignment4/main.go
+++ GoTrainingProgram/Assignments/Assignment4/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func printnumbers() {
+	for i := 1; i <= 5; i++ {
+		time.Sleep(250 * time.Millisecond)
+		fmt.Printf("%d ", i)
+	}
+}
+func printalphabets() {
+	for i := 'a'; i <= 'e'; i++ {
+		time.Sleep(400 * time.Millisecond)
+		fmt.Printf("%c ", i)
+	}
+}
+func main() {
+	go printnumbers()
+	go printalphabets()
+	time.Sleep(2000 * time.Millisecond)
+	fmt.Println("main exit")
+}
